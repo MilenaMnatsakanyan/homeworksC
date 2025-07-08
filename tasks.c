0a846a09560f4c1c4a54a2b481900b1d0897303e
Hovhannisyan00 <ars.hovhannisyan00@gmail.com>
2025-07-08 14:00:53 +0400

    tasks2

---
+++ tasks.c
@@ -0,0 +1,70 @@
+/*
+#include <stdio.h>
+
+    int main()
+{
+    int a = 0;
+    int b = 0;
+    printf("Enter two numbers : ");
+    scanf("%d %d" , &a , &b );
+    int remainder = a%b;
+    printf("%d" , remainder);
+return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter the number : ");
+    scanf("%d" , &number);
+    int cube = number * number * number;
+    printf("%d" , cube );
+return 0;
+}
+
+
+#include <stdio.h>
+
+    int main()
+{
+    int a=0;
+    int b=0;
+    printf("Enter the numbers : ");
+    scanf("%d %d" , &a , &b);
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("%d %d " , a , b );
+return 0;
+}
+
+
+#include <stdio.h>
+#define PI 3.14
+int main()
+{
+    int x=0;
+    printf("Enter the radius : ");
+    scanf("%d" , &x);
+    int area = PI * x * x ;
+    printf("%d" , area);
+    return 0;
+}
+
+
+#include <stdio.h>
+int main () {
+    int a=0;
+    int b=0;
+    int c=0;
+   printf("Enter the numbers :");
+   scanf("%d %d %d" , &a , &b ,&c);
+   int average = (a+b+c)/3;
+   printf("%d" , average);
+   return 0;
+
+
+}
+*/
