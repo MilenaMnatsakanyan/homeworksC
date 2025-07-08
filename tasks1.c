767681599d1c5a118022bf5d763fb140685736b9
Hovhannisyan00 <ars.hovhannisyan00@gmail.com>
2025-07-08 16:24:46 +0400

    updated

---
+++ tasks1.c
@@ -0,0 +1,86 @@
+/*#include <stdio.h>
+int main () {
+    int a = 0;
+    printf("Enter the number : ");
+    scanf("%d" , &a);
+    int opposite = ~a + 1;
+    printf("%d" , opposite);
+    return 0;
+}
+
+#include <stdio.h> 
+int main () {
+    int power = 0;
+    printf("Enter the power : " );
+    scanf("%d" , &power);
+    double n = 1 << power;
+    printf("%2.lf" , n);
+    return 0;
+}
+
+#include <stdio.h>
+int main () {
+    int n = 34;
+    int index = 0;
+    printf("Enter the index : ");
+    scanf("%d" , &index);
+    int x = 1 << index;
+    int y = n | x;
+    printf("%d" , y);
+    return 0;
+}
+
+#include <stdio.h>
+    int main () {
+    int n = 34;
+    int index = 0;
+    printf("Enter the index : ");
+    scanf("%d" , &index);
+    int x = 1 << index;
+    int y = ~x & n;
+    printf("%d" , y);
+    return 0;
+    }
+    #include <stdio.h> 
+int main () {
+    int n = 34;
+    int index = 0;
+    printf("Enter the index : ");
+    scanf("%d" , &index);
+    int x = 1 << index;
+    int y = n | x;
+    printf("%d" , y);
+    return 0;
+}
+
+#include <stdio.h>
+int main() {
+    int x = 0;
+    printf("Enter  the number : ");
+    scanf("%d" , &x);
+    int y = x << 1;
+    printf("%d" , y);
+    return 0;
+} 
+#include <stdio.h> 
+int main () {
+    int x = 0;
+    printf("Enter the number : ");
+    scanf("%d" , &x);
+    int y = x >> 1;
+    printf("%d" , y);
+    return 0;
+}
+#include <stdio.h>
+int main () {
+    int a = 0;
+    int b = 0;
+    printf("Enter the numbers : ");
+    scanf("%d %d" , &a , &b);
+    int x = a ^ b;
+    int f = x ^ a;
+    int v = x ^ b;
+    printf("%d %d" , f , v);
+    return 0;
+}
+*/
